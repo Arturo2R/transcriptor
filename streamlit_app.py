31edb0c47296ce489cd84cfdc0c56993baf30cb1
Arturo Rebolledo <arturoreb2r@outlook.com>
2024-08-17 05:09:39 +0000

    listo apap

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,150 +2,90 @@ import streamlit as st
 import pandas as pd
 import math
 from pathlib import Path
+from pydub import AudioSegment
+import os
+import io
+import subprocess
+from openai import OpenAI
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='Transcripción de Audio',
+    # page_icon=':pen:', # This is an emoji shortcode. Could be a URL too.
 )
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+def split_audio_file(input_file, max_size_mb=20 * 1024 * 1024):
+   # Load the audio file
+    audio = AudioSegment.from_file(input_file)
+    
+    # Get the file extension
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+    ext = "mp3"
+    
+    # Calculate the number of chunks needed
+    file_size = input_file.size
+    num_chunks = math.ceil(file_size / (max_size_mb * 1024 * 1024))
+    
+    # Calculate the duration of each chunk
+    chunk_length_ms = len(audio) // num_chunks
+    files = []
+    # Split and export the audio chunks
+    for i in range(num_chunks):
+        start_time = i * chunk_length_ms
+        end_time = (i + 1) * chunk_length_ms if i < num_chunks - 1 else len(audio)
+        
+        chunk = audio[start_time:end_time]
+        output_filename = f"{input_file.name}_part{i+1}.{ext}"
+        chunk.export(output_filename, format=ext)
+        files.append(output_filename)
+        print(f"Exported {output_filename}")
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    print(f"Audio file split into {num_chunks} parts.")
+    return files
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
 
 # Set the title that appears at the top of the page.
 '''
-# :earth_americas: GDP dashboard
+# :earth_americas: Transcripción de Audio
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
+Solo tienes que poner tu [apikey de openai](https://openai.com) y luego puedes seleccionar cualquier archivo.
+Creado por Arturo Rebolledo.
 '''
 
 # Add some spacing
 ''
 ''
+key = st.text_input(label="Open AI ApiKey")
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+"## Sube Tu Archivo"
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
+client = OpenAI(api_key=key)
 
-countries = gdp_df['Country Code'].unique()
+audio = st.file_uploader(type="mp3", label="Archivo De Audio")
 
-if not len(countries):
-    st.warning("Select at least one country")
+if audio:
+    audio.name
+    st.audio(audio)
+    audio_chunks = split_audio_file(audio)
 
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+    # Saving the chunks to verify the output
+    for chunk in audio_chunks:
+        audio_file = open(chunk, "rb")
+        audio_bytes = audio_file.read()
 
-''
-''
-''
+        transcript = client.audio.transcriptions.create(
+            file=audio_file,
+            model="whisper-1",
+            response_format="text")
+        
+        st.markdown(transcript)
 
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+        os.remove(chunk)
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
